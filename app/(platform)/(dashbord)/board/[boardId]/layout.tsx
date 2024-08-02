f42a6e9629e7149721943ab98a7ea8d71108b345
numdinkushi <numdinkushi@gmail.com>
2024-08-02 09:22:45 +0100

    board page

---
+++ app/(platform)/(dashbord)/board/[boardId]/layout.tsx
@@ -0,0 +1,66 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import BoardNavbar from "./_components/board-navbar";
+
+
+export async function generateMetadata({
+    params
+}: { params: { boardId: string; }; }) {
+    const { orgId } = auth();
+
+    if (!orgId) {
+        return {
+            title: 'Board',
+        };
+    }
+
+    const board = await db?.board?.findUnique({
+        where: {
+            id: params.boardId,
+            orgId,
+        },
+    });
+
+    return {
+        title: board?.title || 'Board',
+    };
+}
+
+const BoardLayout = async ({
+    children,
+    params
+}: {
+    children: React.ReactNode; params: { boardId: string; };
+}) => {
+    const { orgId } = auth();
+
+    if (!orgId) redirect('/select-org');
+
+    const board = await db?.board?.findUnique({
+        where: {
+            id: params.boardId,
+            orgId,
+        },
+    });
+
+    if (!board) {
+        notFound();
+    }
+
+    return (
+        <div
+            className="relative min-h-[100vh] bg-no-repeat bg-cover bg-center"
+            style={{ backgroundImage: `url(${board.imageFullUrl})` }}>
+            <BoardNavbar
+                data={board}
+            />
+            <div className="absolute inset-0 bg-black/10" />
+            <main className="relative pt-28 min-h-[100vh]">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default BoardLayout;
