c45b3d168bbdd042a9f4f338893044a816689cc2
numdinkushi <numdinkushi@gmail.com>
2024-08-03 08:41:30 +0100

    list header

--- app/(platform)/(dashbord)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashbord)/board/[boardId]/_components/list-container.tsx
@@ -8,16 +8,33 @@ interface ListContainerProps {
     boardId: string,
 }
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ListForm from "./list-form";
+import ListItem from "./list-item";
 
 const ListContainer = ({
     data,
     boardId,
 }: ListContainerProps) => {
+    const [orderedData, setOrderedData] = useState(data);
+
+    useEffect(() => {
+        setOrderedData(data);
+    }, [data]);
+
+
     return (
-        <ol>
+        <ol className="flex gap-x-3 h-full ">
             <ListForm />
+            {
+                orderedData.map((list, index) => (
+                    <ListItem
+                        key={index}
+                        index={index}
+                        data={list}
+                    />
+                ))
+            }
             <div className="flex-shrink-0 w-1" />
         </ol>
     );
